6dd948f20eba1534fd6dbcd29262d9d75fa1a737
erick-rios <erickrg2700@ciencias.unam.mx>
2024-02-06 10:48:06 -0600

    first commit

---
+++ java/data_structures/QuickFindUF.java
@@ -0,0 +1,28 @@
+package java.data_structures;
+
+public class QuickFindUF {
+    private int[] id;
+
+    public QuickFindUF(int N){
+        id = new int[N];
+        for(int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+    public boolean connected(int p, int q){
+        return id[p] == id[q];
+    }
+    public void union(int p, int q){
+        int pId = id[p];
+        int qId = id[q];
+        for(int i = 0; i<id.length; i++){
+            if(id[i]== pId){
+                id[i] = qId;
+            }
+        }
+    }
+
+    public int find(int position){
+        return id[position];
+    }
+}
