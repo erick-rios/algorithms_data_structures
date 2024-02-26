a5fec8362070634f444a8ddc8de52c467733e565
erick-rios <erickrg2700@ciencias.unam.mx>
2024-02-26 09:49:12 -0600

    second

--- java/data_structures/assigment/Percolation.java
+++ java/data_structures/assigment/Percolation.java
@@ -1,11 +1,36 @@
 package java.data_structures.assigment;
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    
-    
+    public Percolation(int n){
+
+    }
+
+    public void open(int row, int col){
+        /*
+         * Opens the site (row, col) if it is not open already
+         */
+
+    }
+
+    public boolean isOpen(int row, int col){
+
+    }
+
+    public boolean isFull(int row, int col){
+
+    }
+
+    public int numberOfOpenSites(){
+
+    }
+
+    public boolean percolates(){
+
+    }
+
+    public static void main(String[] args) {
+        
+    } 
 }
